cb080cd73ccdea0f69c88ff8e1fba6253d3a3e69
kevdany17 <kevhernandez07@gmail.com>
2017-11-03 19:54:28 -0600

    Primera Sesion

--- PrimeraSesion/Ejemplo01.c
+++ PrimeraSesion/Ejemplo01.c
@@ -1,9 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
-
-int main(int argc, char *argv[]) {
-	
-	return 0;
+//Varibles Globales
+int numero1;
+void main(int argc, char *argv[]) {
+	//Alfanumericos -> char[0]
+	//Caracter -> char
+	//logicos -> boolean
+	//Decimal -> float. double
+	//Variable es espacio en memoria, cambia con el flujo del programa
+	//Variable Local
+	//Asignar Variable Global
+	numero1 = 2;
+	int numero; //Declarar
+	numero = numero1 + 2; //Asignacion
+	printf("%d",numero);
+	//Funcion -> llama a pause 
+	getch();
 }
+
+
