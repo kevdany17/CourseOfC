c9ab0b42fe09e84279265d6beca59ac9137ea01b
kevdany17 <kevhernandez07@gmail.com>
2017-11-06 15:35:23 -0600

    Add GitIgnore

--- PrimeraSesion/Ejemplo01.c
+++ PrimeraSesion/Ejemplo01.c
@@ -8,9 +8,9 @@ void main(int argc, char *argv[]) {
 	//logicos -> boolean
 	//Decimal -> float. double
 	//Variable es espacio en memoria, cambia con el flujo del programa
-	//Variable Local
 	//Asignar Variable Global
 	numero1 = 2;
+	//Variable Local
 	int numero; //Declarar
 	numero = numero1 + 2; //Asignacion
 	printf("%d",numero);
